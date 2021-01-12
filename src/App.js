9af538e322eacf9bdaeb323412d445785a459e8d
aryonbe <aryonbe@gmail.com>
2021-01-12 12:17:07 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React,{useState,useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+  useEffect(()=>{
+    fetch('/api/time').then(res=>res.json()).then(data=>{
+      setCurrentTime(data.time)
+    });
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
@@ -15,8 +22,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactss
         </a>
+        <p> The current exact time is {currentTime}</p>
       </header>
     </div>
   );
